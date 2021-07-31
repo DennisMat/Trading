cb194f6daa6828a974e089750a8c19c0b96e3430
dennis <dennis.toronto.email@gmail.com>
2021-07-31 15:07:32 -0400

    Working example

--- AIM/src/main/java/com/dennis/aim/Line.java
+++ AIM/src/main/java/com/dennis/aim/Line.java
@@ -1,70 +1,103 @@
 package com.dennis.aim;
 
 public class Line {
-	
-	//int range is -2,147,483,648 to +2,147,483,647
-	
+
+	// int range is -2,147,483,648 to +2,147,483,647
+	int stockPrice;
+
 	int stockValue;
 	int safe;
 	int cash;
+	int sharesBoughtSold;
+	int stockOwned;
 	int portfolioControl;
-	int buyOrSell;
+	int buyOrSellAdvice;
 	int marketOrder;
+	int interest;
 	int portfolioValue;
 	Action action;
 	int Notes;
-	
-	enum Action
-	{
-	    DO_NOTHING, BUY, SELL;
-	}
-	public Line(int stockValue, int prevCash, int prevPortfolioControl, int prevMarketOrder, Action prevAction ) {
-	
-		calculateOtherValues(stockValue, prevCash, prevPortfolioControl, prevMarketOrder, prevAction);
-	
+
+	enum Action {
+		DO_NOTHING, BUY, SELL;
 	}
 
-	public void calculateOtherValues(int stockValue, int prevCash, int prevPortfolioControl, int prevMarketOrder,
-			Action prevAction) {
-		this.stockValue = stockValue;
-		
-		safe=stockValue/10;
-		this.cash = prevCash+prevMarketOrder;
-		
-		if(prevAction==Action.SELL) {
-			portfolioControl= prevPortfolioControl;
-		}else {
-			portfolioControl= prevPortfolioControl + prevMarketOrder/2;
+	public static void printHeader() {
+		String[] headers = { "Stock Price", "Stock Value", "Safe", "Cash", "sharesB&S", "stockOwned",
+				"Portfolio Control", "Buy or Sell Advise", "Market Order", "Interest", "Portfolio Value", "Action" };
+		for (int i = 0; i < headers.length; i++) {
+			System.out.print(headers[i] + "\t");
 		}
-		
-		
-		buyOrSell=stockValue-portfolioControl;
-		
-		
-	int quantityBuySell= safe-buyOrSell;
-	
-	int quantityBuySellAbs= Math.abs(quantityBuySell);
-	
-	if(quantityBuySellAbs>100 && quantityBuySellAbs<safe) {
-		marketOrder=quantityBuySellAbs;
 	}
-	
-	if(marketOrder==0) {
-		action=Action.DO_NOTHING;
-	}else if(quantityBuySell<0) {
-		action=Action.BUY;
-	}else {
-		action=Action.SELL;
+
+	public void printValues() {
+
+		Object[] values = { stockPrice, stockValue, safe, cash, sharesBoughtSold, stockOwned, portfolioControl,
+				buyOrSellAdvice, marketOrder, interest, portfolioValue, action };
+
+		for (int i = 0; i < values.length; i++) {
+			System.out.print(values[i] + "\t");
+		}
+
 	}
-	
-	portfolioValue=stockValue+cash;
+
+	public Line(int stockPrice, int prevSharesBoughtSold, int prevPortfolioControl, int prevMarketOrder,
+			Action prevAction, int prevInterest, int interest) {
+
+		this.stockPrice = stockPrice;
+		this.stockOwned = Main.stockOwned;
+
+		stockValue = this.stockOwned * stockPrice;
+
+		safe = stockValue / 10;
+		Main.cash += prevInterest + prevMarketOrder;
+		this.cash = Main.cash;
+
+		this.interest = interest;
+
+		if (prevAction == Action.SELL) {
+			portfolioControl = prevPortfolioControl;
+		} else {
+			portfolioControl = prevPortfolioControl - prevMarketOrder / 2;
+		}
+
+		portfolioValue = stockValue + cash;
+
+		// if positive sell
+		buyOrSellAdvice = stockValue - portfolioControl;
+
+		// safe is like a threshold, only if abs (buyOrSellAdvice) is above safe you
+		// sell or buy
+		if (safe > Math.abs(buyOrSellAdvice)) {
+			action = Action.DO_NOTHING;
+		} else {
+
+			int quantityBuySell = Math.abs(safe - Math.abs(buyOrSellAdvice));
+
+			if (quantityBuySell > 100) {
+				marketOrder = quantityBuySell;
+			}
+
+			sharesBoughtSold = quantityBuySell / stockPrice;
+			
+			if (marketOrder == 0) {
+				action = Action.DO_NOTHING;
+			} else if (buyOrSellAdvice > 0) {
+				action = Action.SELL;
+				sharesBoughtSold=-sharesBoughtSold;
+			} else if (buyOrSellAdvice < 0) {
+				marketOrder=-marketOrder;
+				action = Action.BUY;
+			}
+			
+			Main.stockOwned += sharesBoughtSold;
+			this.stockOwned = Main.stockOwned;
+		}
+
 	}
-	
-	
-	public void  printValues(){
-		System.out.println(stockValue+"\t" +safe +"\t"+cash 
-				+"\t"+portfolioControl+ "\t"+buyOrSell+ "\t"+marketOrder+ "\t"+portfolioValue+ "\t"+action);
+
+	public Line() {
+		// TODO Auto-generated constructor stub
 	}
-	
 
 }
--- AIM/src/main/java/com/dennis/aim/Main.java
+++ AIM/src/main/java/com/dennis/aim/Main.java
@@ -1,21 +1,62 @@
 package com.dennis.aim;
 
-import com.dennis.aim.Line.Action;
 
 public class Main {
+	
+	
+	public static int stockOwned=500;
+	public static int cash=5000;
 
 	public static void main(String[] args) {
 		
+
+		int[] stockPrice= {8,5,4,5,8,10,8,5,4,5,8,//year 1
+				10,8,5,4,5,8,10,8,5,4,5,8,
+				10,8,5,4,5,8,10,8,5,4,5,8,
+				10,8,5,4,5,8,10,8,5,4,5,8,
+				10,8,5,4,5,8,10,8,5,4,5,8,
+				10,8,5,4,5,8,10,8,5,4,5,8,
+				10,8,5,4,5,8,10,8,5,4,5,8,
+				10,8,5,4,5,8 //year 8
+				};
+		int[] interest= {19,10,2,2,2,17,27,27,15,3,3,24
+				,38,38,22,6,6,34,53,53,31,10,10,48};
 		
-		Line line1 = new Line(5000, 5000, 5000, 0, Line.Action.DO_NOTHING);
 		
-		int[] stockValue= {5500,6000,5600,4400,4560};
 		
-		line1.printValues();
-		Line prevLine=line1;
-		for(int i=0;i<stockValue.length;i++) {
-			Line l= new Line(stockValue[i], prevLine.cash, prevLine.portfolioControl, prevLine.marketOrder,prevLine.action);
+
+		Line lineInt = new Line();
+
+		lineInt.stockPrice = 10;
+		lineInt.stockValue = 5000;
+		lineInt.safe = 500;
+		lineInt.cash = cash;
+		lineInt.sharesBoughtSold = 0;
+		lineInt.stockOwned = stockOwned;
+		lineInt.portfolioControl = 5000;
+		lineInt.buyOrSellAdvice = 0;
+		lineInt.marketOrder = 0;
+		lineInt.interest = 22;
+		lineInt.portfolioValue = 10000;
+		
+		
+		
+		
+		
+		
+		Line.printHeader();
+		System.out.println();
+		lineInt.printValues();
+		System.out.println();
+		
+		
+		Line prevLine=lineInt;
+		for(int i=0;i<interest.length;i++) {
+		//for(int i=0;i<2;i++) {
+			Line l= new Line(stockPrice[i], prevLine.sharesBoughtSold, prevLine.portfolioControl, prevLine.marketOrder,prevLine.action,
+					prevLine.interest,interest[i]);
 			l.printValues();
+			System.out.println();
 			prevLine=l;
 		}
 
